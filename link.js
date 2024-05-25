ba56e0e296fdd842c6751d29aab49cc03aece35d
Shylesh B <144760102+Shylesh51@users.noreply.github.com>
2024-05-25 11:19:23 +0530

    Add files via upload

---
+++ link.js
@@ -0,0 +1,112 @@
+// script.js
+function rezero() {
+    window.location.href = "https://yugenanime.tv/anime/4198/rezero-kara-hajimeru-isekai-seikatsu/";
+}
+
+function horimiya() {
+    window.location.href = "https://yugenanime.tv/anime/7113/horimiya/"
+}
+
+function demon() {
+    window.location.href = "https://yugenanime.tv/anime/4763/kimetsu-no-yaiba/"
+}
+
+function wind() {
+    window.location.href = "https://yugenanime.tv/anime/18971/wind-breaker/"
+}
+
+function shang_chi() {
+    window.location.href = "https://yugenanime.tv/anime/18473/shangri-la-frontier-kusoge-hunter-kamige-ni-idoman-to-su/"
+}
+
+function family() {
+    window.location.href = "https://yugenanime.tv/anime/11900/spy-x-family/"
+}
+
+function magic() {
+    window.location.href = "https://yugenanime.tv/anime/17939/mashle/"
+}
+
+function dbz() {
+    window.location.href = "https://yugenanime.tv/anime/3550/dragon-ball-super/"
+}
+
+function bleach() {
+    window.location.href = "https://yugenanime.tv/anime/15983/bleach-sennen-kessen-hen/"
+}
+
+function hunter() {
+    window.location.href = "https://yugenanime.tv/anime/6379/hunter-x-hunter/"
+}
+
+function gin() {
+    window.location.href = "https://yugenanime.tv/anime/320/gintama/"
+}
+
+function onepiece() {
+    window.location.href = "https://yugenanime.tv/anime/798/one-piece/"
+}
+
+function fmab() {
+    window.location.href = "https://yugenanime.tv/anime/2422/fullmetal-alchemist-brotherhood/"
+}
+
+function naruto() {
+    window.location.href = "https://yugenanime.tv/anime/434/naruto/"
+}
+
+function mushi() {
+    window.location.href = "https://yugenanime.tv/anime/1921/mushishi/"
+}
+
+function drift() {
+    window.location.href = "https://yugenanime.tv/anime/1139/drifters/"
+}
+
+function grimgar() {
+    window.location.href = "https://yugenanime.tv/anime/2601/hai-to-gensou-no-grimgar/"
+}
+
+function grand() {
+    window.location.href = "https://yugenanime.tv/anime/677/grand-blue/"
+}
+
+function relife() {
+    window.location.href = "https://yugenanime.tv/anime/1354/relife/"
+}
+
+function ishura() {
+    window.location.href = "https://yugenanime.tv/anime/18743/ishura/"
+}
+
+function color() {
+    window.location.href = "https://yugenanime.tv/anime/1703/colorful-movie/"
+}
+
+function spirit() {
+    window.location.href = "https://yugenanime.tv/anime/8862/sen-to-chihiro-no-kamikakushi/"
+}
+
+function name() {
+    window.location.href = "https://yugenanime.tv/anime/3881/kimi-no-na-wa/"
+}
+
+function weather() {
+    window.location.href = "https://yugenanime.tv/anime/2374/tenki-no-ko/"
+}
+
+function suzu() {
+    window.location.href = "https://yugenanime.tv/anime/18056/suzume-no-tojimari/"
+}
+
+function mugen() {
+    window.location.href = "https://yugenanime.tv/anime/9924/kimetsu-no-yaiba-mugen-ressha-hen/"
+}
+
+function wolf() {
+    window.location.href = "https://yugenanime.tv/anime/5771/ookami-kodomo-no-ame-to-yuki/"
+}
+
+function howl() {
+    window.location.href = "https://yugenanime.tv/anime/3401/howl-no-ugoku-shiro/"
+}
